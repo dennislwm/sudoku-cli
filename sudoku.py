c65b497b2d18756b14add28023b9b63b242b1a43
lukegrecki <lukegrecki@gmail.com>
2018-01-06 16:51:55 -0500

    Generalized solution and added validation

--- sudoku.py
+++ sudoku.py
@@ -1,12 +1,52 @@
+# stdlib imports
 import copy
+import math
+
+
+class InvalidProblemError(Exception):
+    """An exception raised when:
+
+    1) The problem is not a square grid
+    2) The problem size is not a square number
+    """
 
 
 class Solver:
+    """A class for solving sudoku puzzles.
+    """
+
     def __init__(self, problem):
+        """
+        Initializes the solver class with a given problem. The solution is 
+        initialized as a deep copy of the problem to avoid altering it during 
+        the solving process. It also initializes the problem size and box size 
+        for later use.
+
+        Args:
+            problem (list of lists of ints): This list characterizes a sudoku 
+                grid. The ints are in 0-9, where 0 denotes an empty cell and any
+                other number is a filled cell.
+        """
         self.problem = problem
+        self.size = len(self.problem)
+        self.box_size = int(math.sqrt(self.size))
         self.solution = copy.deepcopy(problem)
 
     def solve(self):
+        """Attempts to solves the sudoku problem recursively with backtracking. 
+        We attempt to fill in each empty cell successively with one of the 
+        available options, and return to change previous cells if we are no 
+        longer able to make valid moves. Having made only valid moves, we stop
+        when the grid is completely filled.
+
+        Returns:
+            The solution grid if successful, False otherwise.
+
+        Raises:
+            InvalidProblemError: If `problem` isn't a valid sudoku grid.
+        """
+        self._validate_problem()
+
         next_empty_cell = self._next_empty_cell()
 
         if not next_empty_cell:
@@ -14,7 +54,7 @@ class Solver:
         else:
             row, column = next_empty_cell
 
-        for number in range(1, 10):
+        for number in range(1, self.size + 1):
             if self._is_valid_move(row, column, number):
                 self.solution[row][column] = number
 
@@ -25,14 +65,24 @@ class Solver:
 
         return False
 
+    def _validate_problem(self):
+        square_root = math.sqrt(self.size)
+        if square_root != self.box_size:
+            raise InvalidProblemError('problem size is not a square')
+
+        for row in self.problem:
+            number_of_columns = len(row)
+            if self.size != number_of_columns:
+                raise InvalidProblemError('problem is not a square grid')
+
     def _is_valid_move(self, row, column, value):
         return not self._used_in_row(row, value) and \
                not self._used_in_column(column, value) and \
                not self._used_in_box(row, column, value)
 
     def _next_empty_cell(self):
-        for row in range(9):
-            for column in range(9):
+        for row in range(self.size):
+            for column in range(self.size):
                 if self.solution[row][column] == 0:
                     return (row, column)
 
@@ -40,7 +90,7 @@ class Solver:
         return value in self.solution[row]
 
     def _used_in_column(self, column, value):
-        for row in range(9):
+        for row in range(self.size):
             if self.solution[row][column] == value:
                 return True
         return False
@@ -55,10 +105,10 @@ class Solver:
         return False
 
     def _find_box(self, row, column):
-        box_row_start = (row / 3) * 3
-        box_rows = range(box_row_start, box_row_start + 3)
+        box_row_start = (row / self.box_size) * self.box_size
+        box_rows = range(box_row_start, box_row_start + self.box_size)
 
-        box_column_start = (column / 3) * 3
-        box_columns = range(box_column_start, box_column_start + 3)
+        box_column_start = (column / self.box_size) * self.box_size
+        box_columns = range(box_column_start, box_column_start + self.box_size)
 
         return (box_rows, box_columns)
--- test_sudoku.py
+++ test_sudoku.py
@@ -1,5 +1,9 @@
+# stdlib imports
 import unittest
-from sudoku import Solver
+import copy
+
+# local imports
+from sudoku import Solver, InvalidProblemError
 
 
 class TestSolver(unittest.TestCase):
@@ -31,6 +35,85 @@ class TestSolver(unittest.TestCase):
         solution = solver.solve()
 
         self.assertEqual(solution, expected_solution)
+
+    def test_it_solves_correctly_for_other_sizes(self):
+        problem = [
+            [0, 1, 0, 0],
+            [4, 0, 0, 3],
+            [1, 0, 0, 2],
+            [0, 0, 3, 0]
+        ]
+        expected_solution = [
+            [3, 1, 2, 4],
+            [4, 2, 1, 3],
+            [1, 3, 4, 2],
+            [2, 4, 3, 1]
+        ]
+
+        solver = Solver(problem)
+        solution = solver.solve()
+
+        self.assertEqual(solution, expected_solution)
+
+    def test_it_does_not_alter_the_problem(self):
+        problem = [
+            [0,9,0,0,0,0,0,0,6],
+            [0,0,0,9,6,0,4,8,5],
+            [0,0,0,5,8,1,0,0,0],
+            [0,0,4,0,0,0,0,0,0],
+            [5,1,7,2,0,0,9,0,0],
+            [6,0,2,0,0,0,3,7,0],
+            [1,0,0,8,0,4,0,2,0],
+            [7,0,6,0,0,0,8,1,0],
+            [3,0,0,0,9,0,0,0,0]
+        ]
+        original_problem = copy.deepcopy(problem)
+
+        solver = Solver(problem)
+        solution = solver.solve()
+
+        self.assertEqual(problem, original_problem)
+
+    def test_returns_false_for_unsolvable_problems(self):
+        unsolvable_problem = [
+            [1,0,2,3,4,5,6,7,8],
+            [0,9,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0]
+        ]
+
+        solver = Solver(unsolvable_problem)
+        solution = solver.solve()
+
+        self.assertEqual(solution, False)
+
+    def test_raises_an_error_for_non_square_problems(self):
+        non_square_problem = [
+            [0,0,0],
+            [0,0,0]
+        ]
+
+        solver = Solver(non_square_problem)
+
+        with self.assertRaises(InvalidProblemError):
+            solver.solve()
+
+
+    def test_raises_an_error_when_the_size_is_not_a_square(self):
+        invalid_sized_problem = [
+            [0,0],
+            [0,0]
+        ]
+
+        solver = Solver(invalid_sized_problem)
+
+        with self.assertRaises(InvalidProblemError):
+            solver.solve()
 
 if __name__ == '__main__':
     unittest.main()
