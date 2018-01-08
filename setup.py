a845ccdc7c81b1368fffe355f509745d57162b24
lukegrecki <lukegrecki@gmail.com>
2018-01-07 20:27:04 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='sudoku-cli',
-    version='0.2',
+    version='0.3',
     description='A CLI for solving Sudoku',
     long_description=long_description,
     url='https://github.com/lukegrecki/sudoku-cli',
