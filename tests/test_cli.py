1662ed37fb8db91ec41251ad9311894d00f1f4cd
lukegrecki <lukegrecki@gmail.com>
2018-01-09 19:59:18 -0500

    Test cli

---
+++ tests/test_cli.py
@@ -0,0 +1,60 @@
+# stdlib imports
+import unittest
+
+# third-party imports
+from click.testing import CliRunner
+
+# local imports
+from sudoku.cli import sudoku
+
+
+class TestCLI(unittest.TestCase):
+    def test_outputs_a_solution(self):
+        runner = CliRunner()
+        result = runner.invoke(sudoku, ['tests/fixtures/problem.txt'])
+        with open('tests/fixtures/solution.txt', 'r') as file:
+            expected_output = file.read()
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, expected_output)
+
+    def test_exits_for_invalid_problems(self):
+        runner = CliRunner()
+        result = runner.invoke(sudoku, ['tests/fixtures/invalid_problem.txt'])
+        expected_output = 'Error: problem is not a square grid on line 1\n'
+
+        self.assertEqual(result.exit_code, 1)
+        self.assertEqual(result.output, expected_output)
+
+    def test_exits_for_unsolvable_problems(self):
+        runner = CliRunner()
+        result = runner.invoke(
+            sudoku,
+            ['tests/fixtures/unsolvable_problem.txt']
+        )
+        expected_output = 'Error: unsolvable problem on line 1\n'
+
+        self.assertEqual(result.exit_code, 1)
+        self.assertEqual(result.output, expected_output)
+
+    def test_ignores_errors_for_invalid_problems(self):
+        runner = CliRunner()
+        result = runner.invoke(
+            sudoku,
+            ['-i', 'tests/fixtures/invalid_problem.txt']
+        )
+        expected_output = '\n'
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, expected_output)
+
+    def test_ignores_errors_for_unsolvable_problems(self):
+        runner = CliRunner()
+        result = runner.invoke(
+            sudoku,
+            ['-i', 'tests/fixtures/unsolvable_problem.txt']
+        )
+        expected_output = '\n'
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, expected_output)
